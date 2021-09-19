7ae33b18b761ecd7fad85a87e5f419352992c85f
Deepak Sharma <TechExhibeo@gmail.com>
2021-09-19 18:07:53 +0530

    Add Automation Script

---
+++ automation.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Colors makes things beautiful
+export TERM=xterm
+
+    red=$(tput setaf 1)             #  red
+    grn=$(tput setaf 2)             #  green
+    blu=$(tput setaf 4)             #  blue
+    cya=$(tput setaf 6)             #  cyan
+    txtrst=$(tput sgr0)             #  Reset
+
+update_repos_and_packages() {
+        echo -e ${blu}"Updating Repos and System"
+        sudo apt update && sudo apt upgrade -y
+        echo -e ${grn}"Repos and System Update completed."${txtrst}
+        wait
+}
+install_apache() {
+        echo -e ${blu}"Apache web server installation"
+        count=`dpkg --get-selections | grep apache | wc -l`
+
+        if [ $count -eq 0 ]
+        then
+                echo -e ${blu}"Installing Apache web server..."${txtrst}
+                sudo apt-get install apache2 -y
+                wait
+		echo -e ${grn}"Apache web server is installed successfully."${txtrst}
+        else
+                echo -e ${grn}"Apache web server already installed."${txtrst}
+        fi
+}
+start_apache(){
+        echo -e ${blu}"Checking if Apache web server is running..."
+        count=`sudo systemctl status apache2 | grep -w active | wc -l`
+        wait
+        if [ $count != 0 ]; then
+                echo -e ${grn}"Apache web server is running."${txtrst}
+        else
+                echo -e ${red}"Apache web server is dead. Starting the daemon..."${txtrst}
+                sudo systemctl start apache2
+                wait
+                echo -e ${grn}"Apache web server is started successfully."${txtrst}
+        fi
+}
+enable_apache() {
+        echo -e ${blu}"Checking if Apache web server is enabled..."
+        count=`sudo systemctl list-unit-files | grep enabled | grep apache | wc -l`
+        wait
+        if [ $count != 0 ]; then
+                echo -e ${grn}"Apache web server is enabled already."${txtrst}
+        else
+                echo -e ${red}"Apache web server is not enabled. Enabling the service..."${txtrst}
+                sudo systemctl enable apache2
+                wait
+                echo -e ${grn}"Apache web server is enabled successfully"${txtrst}
+        fi
+}
+install_awscli() {
+        echo -e ${blu}"AWS CLI installation"${txtrst}
+        count=`dpkg --get-selections | grep awscli | wc -l`
+        if [ $count -eq 0 ]
+        then
+                echo -e ${blu}"Installing AWS CLI..."${txtrst}
+                sudo apt-get install awscli -y
+                wait
+        else
+                echo -e ${grn}"AWS CLI is already installed."${txtrst}
+        fi
+}
+upload_tar_archive() {
+        echo -e ${cya}"Preparing to upload archives to S3..."${txtrst}
+        timestamp=$(date '+%d%m%Y-%H%M%S')
+        myname=Deepak
+        s3_bucket=upgrad-deepak
+        tar -cvf /tmp/$myname-httpd-logs-$timestamp.tar /var/log/apache2/*.log
+        echo -e ${blu}"Uploading archives to S3..."${txtrst}
+        aws s3 cp /tmp/$myname-httpd-logs-$timestamp.tar s3://$s3_bucket/$myname-httpd-logs-$timestamp.tar
+}
+update_repos_and_packages
+install_apache
+start_apache
+enable_apache
+install_awscli
+upload_tar_archive
